4e3c1483d9b20f9f9fcc87c2c95234d55404c1db
Lucas Fernandes <118088988+lucasfernandesdeveloper@users.noreply.github.com>
2024-10-28 05:09:09 -0300

    Arquivos iniciais.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './*.html',
+    './src/**/*.js',
+    './src/**/*.css',
+  ],
+  theme: {
+    extend: { 
+      colors: {
+      'letra-azul': '#264653',
+      'letra-verde': '#398b82',
+      
+    },},
+  },
+  plugins: [],
+}
